ce10cac37fac09bbc2e3c40746bc8c1f7b1162b8
Ilya Leykin <imleykin@gmail.com>
2021-03-21 13:37:04 +0500

    Add db connection

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
 
 const PORT = config.get('port') || 4000;
-
 const app = express();
 
-app.listen(PORT, () => console.log('App has been started'));
+const startApp = async () => {
+  try {
+    await mongoose.connect(config.get('dbUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+
+    app.listen(PORT, () => console.log(`App started on PORT=${PORT}`));
+  } catch(err) {
+    console.log('A server error occured', err.message);
+    process.exit(1);
+  }
+}
+
+startApp();
